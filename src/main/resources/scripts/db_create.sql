0817eef1281b365d67846fb1c1afc7ece5af5b3a
Samuel Moore <samuel.c.moore1221@gmail.com>
2021-07-16 21:20:43 -0500

    Updated db seeder as well as user model

--- src/main/resources/scripts/db_create.sql
+++ src/main/resources/scripts/db_create.sql
@@ -1,35 +1,24 @@
+DROP DATABASE if exists blog_db;
+
 CREATE DATABASE if not exists blog_db;
 
-CREATE USER casey_blog@localhost IDENTIFIED BY 'codeup12';
+# CREATE USER casey_blog@localhost IDENTIFIED BY 'codeup12';
 
-GRANT ALL ON blog_db.* TO casey_blog@localhost;
+GRANT ALL ON blog_db.* TO root@localhost;
 
+USE blog_db;
 
-CREATE TABLE roles
-(
-    id   TINYINT     NOT NULL AUTO_INCREMENT,
-    name VARCHAR(32) NOT NULL,
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE statuses
-(
-    id   TINYINT     NOT NULL AUTO_INCREMENT,
-    name VARCHAR(32) NOT NULL,
-    PRIMARY KEY (id)
-);
 
 CREATE TABLE users
 (
-    id        BIGINT      NOT NULL AUTO_INCREMENT,
-    username  VARCHAR(60) NOT NULL,
-    password VARCHAR(60) NOT NULL,
-    email VARCHAR(200) NOT NULL,
-    role_id   TINYINT     NOT NULL,
-    status_id TINYINT     NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (role_id) REFERENCES roles (id),
-    FOREIGN KEY (status_id) REFERENCES statuses (id)
+    id        BIGINT       NOT NULL AUTO_INCREMENT,
+    username  VARCHAR(60)  NOT NULL,
+    password  VARCHAR(60)  NOT NULL,
+    email     VARCHAR(200) NOT NULL,
+    is_admin  BOOLEAN      NOT NULL,
+    is_active BOOLEAN      NOT NULL,
+    PRIMARY KEY (id)
+
 );
 
 CREATE TABLE posts
@@ -51,9 +40,9 @@ CREATE TABLE tags
 
 CREATE TABLE post_tags
 (
-    id BIGINT NOT NULL AUTO_INCREMENT,
+    id      BIGINT NOT NULL AUTO_INCREMENT,
     post_id BIGINT NOT NULL,
-    tag_id INT NOT NULL,
+    tag_id  INT    NOT NULL,
     PRIMARY KEY (id),
     FOREIGN KEY (post_id) REFERENCES posts (id),
     FOREIGN KEY (tag_id) REFERENCES tags (id)
